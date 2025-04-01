e1590489f6b85f4324d63b359f189ad05b935a3e
Chrysalid07 <168845197+Chrysalid07@users.noreply.github.com>
2025-04-01 12:35:55 +0200

    Changed the news text to black

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -7,34 +7,34 @@ export default function Page() {
                 <Link className="rounded-lg" href="/">
                     <img className="w-10 h-10" src="/Pred images/Flecha.png" />
                 </Link>
-                <h1 className="text-7xl font-semibold">Waifu News</h1>
+                <h1 className="text-7xl text-black font-semibold">Waifu News</h1>
             </div>
             <div className="p-4">
-                <h2 className="text-5xl font-semibold">1.0</h2>
+                <h2 className="text-5xl text-black font-semibold">1.0</h2>
                 <div className="flex flex-col gap-8 p-4 bg-black/5 rounded-lg w-fit p-6">
-                    <h3 className="text-3xl font-semibold">Novedades:</h3>
+                    <h3 className="text-3xl text-black font-semibold">Novedades:</h3>
                     <div className="">
-                        <p className="text-4xl font-bold rounded-lg w-fit font-bold p-2">1.0 RELEASE!</p>
-                        <p className="text-xl">The official release is finally out!</p>
+                        <p className="text-4xl text-black font-bold rounded-lg w-fit font-bold p-2">1.0 RELEASE!</p>
+                        <p className="text-xl text-black">The official release is finally out!</p>
                     </div>
                     <div>
-                        <p className="text-xl">A new Waifu will be added after a certain time.</p>
-                        <p className="text-xl">Always remember to look up the page.</p>
-                        <p className="text-xl">There are some clues for future waifus!</p>
+                        <p className="text-xl text-black">A new Waifu will be added after a certain time.</p>
+                        <p className="text-xl text-black">Always remember to look up the page.</p>
+                        <p className="text-xl text-black">There are some clues for future waifus!</p>
                     </div>
                     <div className="flex flex-col gap-4">
-                        <h4 className="text-2xl font-bold rounded-lg w-fit">New Waifu:</h4>
-                        <p className="text-2xl rounded-lg w-fit">· Added Yoimiya!</p>
+                        <h4 className="text-2xl text-black font-bold rounded-lg w-fit">New Waifu:</h4>
+                        <p className="text-2xl text-black rounded-lg w-fit">· Added Yoimiya!</p>
                     </div>
                     <div className="flex flex-col gap-4">
-                        <h4 className="text-2xl font-bold rounded-lg w-fit">Additions:</h4>
-                        <p className="text-2xl rounded-lg w-fit">· Added the Waifu carrousel</p>
-                        <p className="text-2xl rounded-lg w-fit">· A mechanic has finally been added to Ellen</p>
-                        <p className="text-2xl rounded-lg w-fit">· Added a new section: THE WAIFU NEWS</p>
+                        <h4 className="text-2xl text-black font-bold rounded-lg w-fit">Additions:</h4>
+                        <p className="text-2xl text-black rounded-lg w-fit">· Added the Waifu carrousel</p>
+                        <p className="text-2xl text-black rounded-lg w-fit">· A mechanic has finally been added to Ellen</p>
+                        <p className="text-2xl text-black rounded-lg w-fit">· Added a new section: THE WAIFU NEWS</p>
                     </div>
                     <div className="flex flex-col gap-4">
-                        <h4 className="text-2xl font-bold rounded-lg w-fit">Improvements:</h4>
-                        <p className="text-2xl rounded-lg w-fit">· Improved the Navbar and the respective icons </p>
+                        <h4 className="text-2xl text-black font-bold rounded-lg w-fit">Improvements:</h4>
+                        <p className="text-2xl text-black rounded-lg w-fit">· Improved the Navbar and the respective icons </p>
                     </div>
                 </div>
             </div>
